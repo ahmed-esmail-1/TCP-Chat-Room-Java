a0c7526371e594c3b9d2b1d465ddd6a7a91142bd
Ahmed Esmail <ahmedalawi01@gmail.com>
2023-09-17 00:17:14 +0300

    Implement client handler

--- src/tcp/chat/room/Server.java
+++ src/tcp/chat/room/Server.java
@@ -1,8 +1,14 @@
 
 package tcp.chat.room;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Server implements Runnable {
     
@@ -12,17 +18,40 @@ public class Server implements Runnable {
         try
         {
             ServerSocket server = new ServerSocket(8898);
+            Socket client = server.accept();
+            
+            
         }catch (IOException e)
         {
-            
+            //
         }
     }
     
     
     class ConnectionHandler implements Runnable {
+        
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String username;
+        
+        public ConnectionHandler(Socket client){
+            this.client = client;
+        }
+        
         @Override
         public void run(){
-
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Please Enter a Username: ");
+                username = in.readLine();
+                System.out.println(username + " Connected!");
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            
         }
     }
     
